25ac07d31d12876b2ea259bf2899aa882edd05c6
Jose Mondragon <121974632+mondragon-developer@users.noreply.github.com>
2024-08-07 16:02:34 -0400

    fixing routing in app.js-navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
